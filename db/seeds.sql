4e8a71a415743f5f4fff809db7a323c37a763e9e
Marble <mrivera0014@gmail.com>
2021-02-28 16:50:04 -0700

    began folder structure. Needed modules installed db folder/files created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('Pretty Patty');
+INSERT INTO burgers (burger_name) VALUES ('Krabby Double Deluxe')
