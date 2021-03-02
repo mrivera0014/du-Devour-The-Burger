79524d58a94b7b653f8ea74dee46683a78f39875
Marble <mrivera0014@gmail.com>
2021-03-01 23:41:05 -0700

    preparing to deploy to heroku, connection.js file modified

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
 const mysql = require('mysql');
 
 //connecting to mysql
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Canelo!14',
-    database: 'burgers_db'
-})
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'Canelo!14',
+//     database: 'burgers_db'
+// })
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Canelo!14',
+        database: 'burgers_db'
+    })
+}
 
 //make connection
 connection.connect((err) => {
